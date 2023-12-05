2b1322f204fdcf1e632d230f7c9bf91cfdeb5b04
changyu <changyu1061@naver.com>
2023-12-05 10:38:27 +0900

    yml 파일 삭제.

---
+++ src/main/java/com/example/todo/userapi/entity/User.java
@@ -0,0 +1,51 @@
+package com.example.todo.userapi.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "tbl_user")
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id; // 계정명이 아니라 식별 코드
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @CreationTimestamp
+    private LocalDateTime joinDate;
+
+    @Enumerated(EnumType.STRING)
+//    @ColumnDefault("'COMMON'") // Enum타입으로 안쪽에 홑따옴표.
+    @Builder.Default
+    private Role role = Role.COMMON;
+
+    private String profileImg; // 프로필 이미지 경로
+
+    // 등급 수정 메서드
+    public void changeRole(Role role) {
+        this.role = role;
+    }
+
+}
